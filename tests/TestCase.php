085fa224822ce215718b48a8e34a39a8a5db51e5
Sidney Oliveira <oliveira.sidney96@gmail.com>
2020-10-22 22:51:27 -0300

    Add namespace in test folder

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,14 +1,11 @@
 <?php
 
+namespace Tests;
+
 use Laravel\Lumen\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    /**
-     * Creates the application.
-     *
-     * @return \Laravel\Lumen\Application
-     */
     public function createApplication()
     {
         return require __DIR__.'/../bootstrap/app.php';
